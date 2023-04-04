135d953fbdb475ef7673d716feb83a4ad94621bf
aleksandrsudakov <alexdsa@yandex.ru>
2023-04-04 15:27:01 +0300

    The first commit of spring application

---
+++ src/main/java/ru/sudakov/springfirst/TestBean.java
@@ -0,0 +1,17 @@
+package ru.sudakov.springfirst;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
